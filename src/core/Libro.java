9bd4e96fa36850b76355ccf696193d954f6ab789
mariaher05th <fawcettm@uninorte.edu.co>
2024-04-01 12:58:51 -0500

    AYUDAAAA

---
+++ src/core/Libro.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author maryj
+ */
+public abstract class Libro {
+    protected String titulo;
+    protected ArrayList<Autor> Autor;
+    protected String isbn;
+    protected String genero;
+    protected String formato;
+    protected double valor;
+    protected Editorial editorial;
+    
+    
+    
+    
+    
+    
+}
